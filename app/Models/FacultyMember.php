6d99078eb5adb480ccbe35178ea80fa8bdf8704a
Arwa Sarrar <442301509@nu.edu.sa>
2025-03-26 05:08:34 +0300

    Add JWT to FM

--- app/Models/FacultyMember.php
+++ app/Models/FacultyMember.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Model;
 
-class FacultyMember extends Model
+class FacultyMember extends Model implements JWTSubject
 {
+    use HasFactory, Notifiable;
+
     protected $fillable = [
         'pass',
         'f_name',
@@ -22,4 +28,26 @@ class FacultyMember extends Model
         'agent_name',
 
     ];
+
+    use Notifiable;
+
+    // Rest omitted for brevity
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
