82af4c2f3163f923ed3558b25d2c67b65bdf4f19
Jomanh S Alqahtani <442408932@nu.edu.sa>
2025-04-09 02:29:08 +0300

    Added Updated Requests Interfaces

--- public/hakkem/javascript/Review-Request.js
+++ public/hakkem/javascript/Review-Request.js
@@ -7,90 +7,90 @@ const confirmBtn = document.querySelector(".confirm");
 let currentStep = 0;
 
 function updateStepProgress() {
-  steps.forEach((step, index) => {
-    if (index < currentStep) {
-      step.classList.remove("preactive");
-      step.classList.add("active");
-    } else if (index === currentStep) {
-      step.classList.add("preactive");
-      step.classList.remove("active");
-    } else {
-      step.classList.remove("preactive", "active");
-    }
-  });
+    steps.forEach((step, index) => {
+        if (index < currentStep) {
+            step.classList.remove("preactive");
+            step.classList.add("active");
+        } else if (index === currentStep) {
+            step.classList.add("preactive");
+            step.classList.remove("active");
+        } else {
+            step.classList.remove("preactive", "active");
+        }
+    });
 }
 
 function showStep() {
-  formSteps.forEach((step, index) => {
-    step.classList.toggle("active", index === currentStep);
-  });
-  updateStepProgress();
+    formSteps.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep);
+    });
+    updateStepProgress();
 }
 
 nextBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (currentStep < formSteps.length - 1) {
-      currentStep++;
-      showStep();
-    }
-  });
+    button.addEventListener("click", () => {
+        if (currentStep < formSteps.length - 1) {
+            currentStep++;
+            showStep();
+        }
+    });
 });
 
 backBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (currentStep > 0) {
-      currentStep--;
-      showStep();
-    }
-  });
+    button.addEventListener("click", () => {
+        if (currentStep > 0) {
+            currentStep--;
+            showStep();
+        }
+    });
 });
 
 confirmBtn.addEventListener("click", () => {
-  alert("Request Completed!");
+    alert("Request Completed!");
 });
 
 function displayFileName() {
-  const fileInput = document.getElementById("files");
-  const fileName = fileInput.files[0]
-    ? fileInput.files[0].name
-    : "No file selected";
-  document.getElementById("file-name").textContent = fileName;
+    const fileInput = document.getElementById("files");
+    const fileName = fileInput.files[0]
+        ? fileInput.files[0].name
+        : "No file selected";
+    document.getElementById("file-name").textContent = fileName;
 }
 
 function profileCard() {
-  var userId = document.getElementById("userId").value;
-  var optionsDiv = document.getElementById("user");
+    var userId = document.getElementById("userId").value;
+    var optionsDiv = document.getElementById("user");
 
-  if (userId === "123") {
-    // هذا مثال مبدئي، راح يختلف بعد ما ينربط بالداتابيس
-    fetch("usercontainer.html") // تحميل ملف HTML الجانبي
-      .then((response) => response.text()) // تحويله إلى نص
-      .then((data) => {
-        optionsDiv.innerHTML = data; // عرض المحتوى داخل div
-        optionsDiv.classList.add("active");
-      })
-      .catch((error) => console.error("Can not upload", error));
-  } else {
-    optionsDiv.innerHTML = "<p>No options available for this ID</p>";
-  }
+    if (userId === "123") {
+        // هذا مثال مبدئي، راح يختلف بعد ما ينربط بالداتابيس
+        fetch("usercontainer")
+            .then((response) => response.text()) // تحويله إلى نص
+            .then((data) => {
+                optionsDiv.innerHTML = data; // عرض المحتوى داخل div
+                optionsDiv.classList.add("active");
+            })
+            .catch((error) => console.error("Can not upload", error));
+    } else {
+        optionsDiv.innerHTML = "<p>No options available for this ID</p>";
+    }
 }
 
 document.getElementById("price-input").addEventListener("input", function () {
-  document.getElementById("output-price").innerText = this.value;
+    document.getElementById("output-price").innerText = this.value;
 });
 
 function check() {
-  field = document.getElementById("price-input");
-  error = document.getElementById("err");
-  if (!field.checkValidity()) {
-    valid = false;
-    field.classList.add("err");
-    error.innerHTML = "Price must be less than 50,000.00 SAR";
-  } else {
-    field.classList.remove("err");
-    error.innerHTML = "";
-  }
-  return valid;
+    field = document.getElementById("price-input");
+    error = document.getElementById("err");
+    if (!field.checkValidity()) {
+        valid = false;
+        field.classList.add("err");
+        error.innerHTML = "Price must be less than 50,000.00 SAR";
+    } else {
+        field.classList.remove("err");
+        error.innerHTML = "";
+    }
+    return valid;
 }
 
 showStep();
